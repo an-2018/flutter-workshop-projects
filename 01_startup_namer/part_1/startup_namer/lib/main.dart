0a572adc7bca17f5e13b8e08f341c0e40fd85b68
anilson <anilsonmonteiro@gmail.com>
2022-05-03 11:08:18 -0100

    feat(startup namer): add infinite scrolling listview

--- 01_startup_namer/part_1/startup_namer/lib/main.dart
+++ 01_startup_namer/part_1/startup_namer/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HelloApp());
+  runApp(const HelloApp());
 }
 
 class HelloApp extends StatelessWidget {
@@ -11,14 +11,15 @@ class HelloApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Wellcome to the Flutter Workshop",
+      title: "Startup Name Generator",
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Is it The Flutter Workshop"),
-          ),
-          body: const Center(
-            child: RandomWords(),
-          )),
+        appBar: AppBar(
+          title: const Center(child: Text("Startup Name Generator")),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
     );
   }
 }
@@ -31,9 +32,26 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
   }
 }
