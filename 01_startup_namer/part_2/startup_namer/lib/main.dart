8806764ec8d26002abfba1753f10250a83066a23
anilson <anilsonmonteiro@gmail.com>
2022-05-04 13:35:34 -0100

    feat(startup namer): part2 change color of app bar theme

--- 01_startup_namer/part_2/startup_namer/lib/main.dart
+++ 01_startup_namer/part_2/startup_namer/lib/main.dart
@@ -10,9 +10,15 @@ class HelloApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Startup Name Generator",
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
       home: RandomWords(),
     );
   }
