88439fa4d1051266120d62d11eef3d0c67ebf331
anilson <anilsonmonteiro@gmail.com>
2022-05-11 16:01:22 -0100

    feat(andela_website): add initial flutter app

---
+++ 05_andela_website_clone/andela_website/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        elevation: 0,
+      ),
+      body: Container(
+        color: Colors.purple,
+      ),
+    );
+  }
+}
