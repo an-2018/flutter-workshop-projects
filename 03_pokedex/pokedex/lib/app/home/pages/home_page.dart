d2f9e22f0d859365b8ac8b376a5dff661d1e1fea
anilson <anilsonmonteiro@gmail.com>
2022-05-11 14:20:33 -0100

    feat(pokedex): add home page layout improvement

--- 03_pokedex/pokedex/lib/app/home/pages/home_page.dart
+++ 03_pokedex/pokedex/lib/app/home/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/app/details/pages/container/detail_container.dart';
+import 'package:pokedex/app/home/pages/widgets/pokemn_card_widget.dart';
 import 'package:pokedex/common/models/Pokemon.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,16 +12,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: pokemons.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-                onTap: () => onItemTap(
-                      "/details",
-                      DetailArguments(name: pokemons[index].name),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Padding(
+          padding: EdgeInsets.only(left: 8.0),
+          child: Text(
+            "Pokedex",
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                )),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+          children: pokemons
+              .map((e) => PokemonCard(
+                    pokemon: e,
+                    onTap: () => onItemTap(
+                      '/details',
+                      DetailArguments(name: e.name),
                     ),
-                title: Text(pokemons[index].name));
-          }),
+                  ))
+              .toList(),
+        ),
+      ),
     );
   }
 }
