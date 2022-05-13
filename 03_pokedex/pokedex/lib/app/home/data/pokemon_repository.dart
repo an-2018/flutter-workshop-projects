5bff5624c0ccd708b01f171ea4e5a3781851d063
anilson <anilsonmonteiro@gmail.com>
2022-05-13 15:39:46 -0100

    feat(pokedex): add initial network implemntation

--- 03_pokedex/pokedex/lib/app/home/data/pokemon_repository.dart
+++ 03_pokedex/pokedex/lib/app/home/data/pokemon_repository.dart
@@ -9,23 +9,22 @@ abstract class IPokemonRepository {
 }
 
 class PokemonRepository implements IPokemonRepository {
+  //https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json
   Uri url = Uri.parse(ApiConstants.allPokemonURL);
 
   @override
   Future<List<Pokemon>> getAll() async {
-    try {
-      final response = await http.get(url);
-
-      if (response.statusCode >= 200 && response.statusCode < 300) {
-        final json = jsonDecode(response.body) as Map<String, dynamic>;
-        final list = json['pokemon'] as List<dynamic>;
-        return list.map((e) => Pokemon.fromMap(e)).toList();
-      }
-    } catch (e) {
-      throw Failure('Nao foi possivel carregar os dados');
-    }
-    return Future.delayed(
-      Duration(seconds: 1),
+    final pokemons = <Pokemon>[
+      Pokemon(
+        id: 1,
+        image: "http://www.serebii.net/pokemongo/pokemon/001.png",
+        name: 'Bulbassaur',
+        num: '1',
+        type: ["Grass", "Poison"],
+      ),
+    ];
+    return Future.value(
+      pokemons,
     );
   }
 }
