e532da1a673e30eb080d3ffb477b4869691cc570
anilson <anilsonmonteiro@gmail.com>
2022-05-13 13:30:02 -0100

    feat(workshopsession1) add initial startup namer struture

---
+++ 00_the_event/startup_name/startup_namer/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: RandomWords()
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({ Key? key }) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final WordPair pair = WordPair.random();
+    return Text(pair.asPascalCase);
+  }
+}
